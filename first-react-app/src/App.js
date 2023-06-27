2204e66ae92f477ef6d7fc79b4dde377ef48bec6
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-27 22:48:23 +0900

    useEffect

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,11 +1,15 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("I run all the time");
+  // 코드가 한번만 실행되도록 보호
+  useEffect(() => console.log("I run only once"), []);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
