2bed274629be72fb9af2a47177b1fa8a224bb4fc
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-28 21:48:17 +0900

    Movie app (error fix)

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,6 +1,24 @@
-import { useEffect, useState } from "react";
-import Movie from "./components/Movie";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Detail from "./routes/Detail";
+import Home from "./routes/Home";
 
-function App() {}
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/hello">
+          <h1>Hello</h1>
+        </Route>
+        <Route path="/movie">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
 
 export default App;
--- first-react-app/src/components/Movie.js
+++ first-react-app/src/components/Movie.js
@@ -1,10 +1,14 @@
+import React from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 function Movie({ coverImg, title, summary, genres }) {
   return (
     <div>
       <img src={coverImg} alt={title} />
-      <h2>{title}</h2>
+      <h2>
+        <Link to="/movie">{title}</Link>
+      </h2>
       <p>{summary}</p>
       <ul>
         {genres.map((g) => (
@@ -14,12 +18,10 @@ function Movie({ coverImg, title, summary, genres }) {
     </div>
   );
 }
-
-Movie.PropTypes = {
+Movie.propTypes = {
   coverImg: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   summary: PropTypes.string.isRequired,
   genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
-
 export default Movie;
