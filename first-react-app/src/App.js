fc62a1e002d59b130b5e382afe08395ec56a1a9a
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-28 14:57:57 +0900

    cleanup

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,38 +1,23 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  useEffect(() => {
+    console.log("hi");
+    // cleanUp 코드 (해당 함수가 파괴될 때 실행)
+    return () => {
+      console.log("bye");
+    };
+  }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  console.log("I run all the time");
-  // 코드가 배열에 들어있는 값이 변화할 때만 실행
-  useEffect(() => {
-    if (keyword !== "" && keyword.length >= 5) {
-      console.log("SEARCH FOR", keyword);
-    }
-  }, [keyword]);
-  // 코드가 한번만 실행되도록 보호
-  useEffect(() => console.log("I run only once"), []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when 'keyword' & 'counter' changes.");
-  }, [keyword, counter]);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here..."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
