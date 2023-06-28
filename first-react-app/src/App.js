a3a5405bb8a798b3159fc43ef2cdb1fc47e3fb90
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-28 14:39:30 +0900

    useEffect 2

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -2,12 +2,35 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("I run all the time");
+  // 코드가 배열에 들어있는 값이 변화할 때만 실행
+  useEffect(() => {
+    if (keyword !== "" && keyword.length >= 5) {
+      console.log("SEARCH FOR", keyword);
+    }
+  }, [keyword]);
   // 코드가 한번만 실행되도록 보호
   useEffect(() => console.log("I run only once"), []);
+  useEffect(() => {
+    console.log("I run when 'keyword' changes.");
+  }, [keyword]);
+  useEffect(() => {
+    console.log("I run when 'counter' changes.");
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [keyword, counter]);
   return (
     <div>
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search here..."
+      />
       <h1>{counter}</h1>
       <button onClick={onClick}>click me</button>
     </div>
