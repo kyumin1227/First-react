dcae82a0b717b355ec4f99369c1fc62e88c545ed
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-28 19:34:48 +0900

    Movie app 1

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,63 +1,6 @@
 import { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
-function Info() {
-  return <span>{} ~ 구매 가능</span>;
-}
-
-function App() {
-  const [loading, setLoading] = useState(true);
-  const [inputMoney, setInputMoney] = useState(false);
-  const [coins, setCoins] = useState([]);
-  const [money, setMoney] = useState("");
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => setCoins(json));
-    setLoading(false);
-  }, []);
-  const onChange = (event) => {
-    console.log(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    console.log("okay");
-    setInputMoney(true);
-  };
-  const onChangeMoney = (event) => {
-    console.log(event.target.value);
-    setMoney(event.target.value);
-  };
-  return (
-    <div>
-      <h1>The Coin! {coins.length !== 0 ? `(${coins.length})` : null}</h1>
-      {}
-      <br></br>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : inputMoney ? (
-        <h4>보유 자산: ${money}</h4>
-      ) : (
-        <form onSubmit={onSubmit}>
-          <input
-            onChange={onChangeMoney}
-            value={money}
-            type="number"
-            placeholder="My money"
-          ></input>
-          <button>okay</button>
-        </form>
-      )}
-      <select onChange={onChange}>
-        <option value={0}>Select Coin!</option>
-        {coins.map((coin) => (
-          <option value={coin.id}>
-            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} 구매 가능 수량{" "}
-            {money / coin.quotes.USD.price}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-}
+function App() {}
 
 export default App;
