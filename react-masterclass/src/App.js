d32c654557ea6aaff8a0b45a9cac0c5d0b33104c
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-30 00:23:09 +0900

    Styled component 3

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -1,24 +1,57 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
 `;
 
-const Input = styled.input.attrs({required: true, minLength: 10})`
-  background-color: "tomato";
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform:rotate(360deg);
+    border-radius: 0px;
+  }
+`;
+
+const Emoji = styled.span`
+  font-size: 36px;
+  &:active {
+    opacity: 0;
+  }
+`;
+
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${animation} 1s linear infinite;
+  ${Emoji} {
+    &:hover {
+      font-size: 100px;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as="header" >
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+    <Father>
+      <Box>
+        <Emoji>😭</Emoji>
+      </Box>
+      <Emoji>🙈</Emoji>
     </Father>
-  )
-};
+  );
+}
 
 export default App;
