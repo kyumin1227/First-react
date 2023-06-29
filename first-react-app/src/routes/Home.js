3c1c936a41640696f9ed090ac003e22de2415378
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-29 23:17:08 +0900

    Append css

--- first-react-app/src/routes/Home.js
+++ first-react-app/src/routes/Home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import Movie from "../components/Movie";
+import style from "../css/Home.module.css";
 
 function Home() {
   const [loading, setLoading] = useState(true);
@@ -21,7 +22,7 @@ function Home() {
       {loading ? (
         <h1>Loading...</h1>
       ) : (
-        <div>
+        <div className="movies">
           {movies.map((movie) => (
             <Movie
               key={movie.id}
