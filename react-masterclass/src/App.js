ea480d547b3bf41bcdc7b90f2c59ce9f35f85b4d
kyumin1227 <68456336+kyumin1227@users.noreply.github.com>
2023-06-29 23:48:28 +0900

    Styled component 2

--- react-masterclass/src/App.js
+++ react-masterclass/src/App.js
@@ -4,34 +4,21 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-// props를 이용한 속성 값 지정
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
+const Input = styled.input.attrs({required: true, minLength: 10})`
+  background-color: "tomato";
 `;
-
-// Box값을 상속 (기존 값 + 추가한 값)
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-
-const Btn = styled.button`
-  background-color: tomato;
-  color: white;
-  border: 0;
-  border-radius: 15px;
-`
-
-
 
 function App() {
   return (
-    <Father>
-      <Btn>Log in</Btn>
-      <Btn as="a" href="/">Log in</Btn>
+    <Father as="header" >
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
+      <Input />
     </Father>
-  );
-}
+  )
+};
 
 export default App;
